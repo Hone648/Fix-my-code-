3cc8f0f55488d2081a2be5cc33a8f31a83719c80
Hunter Kam <hone648@gmail.com>
2020-02-18 18:45:45 -0800

    Fixed

---
+++ bugs.js
@@ -0,0 +1,23 @@
+var name = "Robert";
+var nickName = "Bob";
+var petName = "Fluffy";
+var petType = "Turtle";
+if(name == "Robert"){
+    console.log("Hi Robert");
+}
+else if(name == "Bob"){
+    console.log("Hi Bob");
+}
+else if(name != "Robert"){
+    console.log("Hello Stranger");
+}
+if(petType == "Dog"){
+    console.log("Time to play fetch");
+}
+if(petType = "Cat"){
+    console.log("Time to play with a laser pointer");
+}
+else
+    console.log("Time to play with my pet");
+
+
